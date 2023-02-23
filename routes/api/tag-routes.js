87d52c5a28573aeba070d091b816778f9ff8324e
John Kang <johnkang1121@gmail.com>
2023-02-22 16:09:05 -0800

    added code to get and post in tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,13 +21,42 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Product,
+        },
+      ],
+    });
+    if (!tagData) {
+      res
+        .status(404)
+        .json({
+          message: "Category with the id " + req.params.id + "is not found!",
+        });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  }catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
